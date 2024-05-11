1c15cade2c5ca6ef61d248f77a8a24ae3df257d6
Roxana1981 <roxanaameijide81@gmail.com>
2024-05-11 16:54:12 +0000

    Remov env file and aws deployment

--- thevintagestore/env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = "04%um&n0*r$@n)ucvbns4u(_b$u4_p!&2ebev7o0#+h--j$72-"
-os.environ["DATABASE_URL"] = "postgres://orznnpbm:vRKXhQyMZTV7i3d32G_VgZhKIsVhXSzD@tai.db.elephantsql.com/orznnpbm"
-os.environ["DEVELOPMENT"] = "True"
