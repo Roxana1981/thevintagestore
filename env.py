2e9c826a686e3e0f39b2317e67e43466bf08566b
Roxana1981 <roxanaameijide81@gmail.com>
2024-05-11 16:35:04 +0000

    AWS added for next deployment release

--- env.py
+++ env.py
@@ -1,5 +1,7 @@
 import os
 
+os.environ["AWS_ACCESS_KEY_ID"] = "AKIARVNBVRMBTN6ENNY2"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
 os.environ["SECRET_KEY"] = "04%um&n0*r$@n)ucvbns4u(_b$u4_p!&2ebev7o0#+h--j$72-"
 os.environ["DATABASE_URL"] = "postgres://orznnpbm:vRKXhQyMZTV7i3d32G_VgZhKIsVhXSzD@tai.db.elephantsql.com/orznnpbm"
 os.environ["DEVELOPMENT"] = "True"
